4d89ebc49779ca3f859b1e5a4adb916fd8099ede
0xDLT <30773517+0xDLT@users.noreply.github.com>
2024-10-19 22:57:37 +0300

    📙 FIX: redirect

--- reservations/booking.php
+++ reservations/booking.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['id'])) {
             $stmt->execute([$user_id, $booking_datetime, $number_of_people, $special_requests]);
 
             $_SESSION['alert'] = 'Booked a table';
-            header('location: ../app/index.php');
+            header('location: ../app/booking.php');
             exit();
         } 
     }
@@ -28,7 +28,4 @@ if (isset($_SESSION['alert'])) {
     echo "<script>alert('" . $_SESSION['alert'] . "');</script>";
     unset($_SESSION['alert']); // Clear the message after displaying it
 }
-
-header("location: ../app/App.php");
-exit();
 ?>
